ffbf19b23b4258f3c4b87e6ef772d6b3a4050b61
Askar Pulatov <master15994@gmail.com>
2023-09-25 14:46:41 +0300

    add searchValue && add new contacts

--- src/App.js
+++ src/App.js
@@ -7,13 +7,37 @@ import { Users } from './componets/Users/Users';
 // Тут список пользователей: https://reqres.in/api/users
 
 const App = () => {
+  const [users, setUsers] = useState([])
+  const [searchValue, setSearchValue] = useState('')
+  const [isLoading, setLoading] = useState(true)
+
+  const onChangeSearchValue = (event) => {
+    setSearchValue(event.target.value)
+  }
+
+  useEffect(() => {
+    fetch('https://reqres.in/api/users')
+      .then(res => res.json())
+      .then(json => {
+        setUsers(json.data)
+      })
+      .catch(err => {
+        console.log(err);
+        alert('Ошибка')
+      })
+      .finally(() => setLoading(false))
+  }, [])
 
   return (
     <div
       className="App" >
-      < Users />
+      < Users
+        searchValue={searchValue}
+        onChangeSearchValue={onChangeSearchValue}
+        items={users}
+        isLoading={isLoading} />
 
-      <Success />
+      {/* <Success /> */}
 
     </div>);
 }
